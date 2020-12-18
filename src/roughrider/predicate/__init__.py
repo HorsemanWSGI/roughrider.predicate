863953aa1ad4be34d1273e3f685af7428ca1cdb4
Souheil Chelfouh <trollfot@gmail.com>
2020-12-18 15:34:24 +0100

    first commit

---
+++ src/roughrider/predicate/__init__.py
@@ -0,0 +1,11 @@
+from .validation import OR, Validator, resolve_validators
+from .validation import Error, ConstraintsErrors
+
+
+__all__ = [
+    'Error',
+    'ConstraintsErrors',
+    'OR',
+    'Validator',
+    'resolve_validators'
+]
