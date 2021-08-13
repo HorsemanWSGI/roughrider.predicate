0e10401a0f6243ba9ccc00c7ac3e0bbec4ee3cce
Souheil Chelfouh <trollfot@gmail.com>
2021-08-13 16:13:19 +0200

    Corrected decorator to accept all kind of errors.

--- src/roughrider/predicate/decorator.py
+++ src/roughrider/predicate/decorator.py
@@ -1,19 +1,22 @@
 from functools import wraps
-from typing import Callable, Any
-from roughrider.predicate.errors import ConstraintError
+import typing as t
+from roughrider.predicate.errors import ConstraintError, ConstraintsErrors
 
 
-PredicateErrorHandler = Callable[[ConstraintError], Any]
+Error = t.Union[HTTPConstraintError, ConstraintError, ConstraintsErrors]
+Predicates = t.Iterable[t.Callable[[...], t.Any]
+PredicateErrorHandler = t.Callable[[Error], t.Any]
 
 
-def with_predicates(predicates, handler=None):
+def with_predicates(
+        predicates: Predicates, handler: PredicateErrorHandler = None):
     def predication_wrapper(func):
         @wraps(func)
         def assert_predicates(*args, **kwargs):
             for predicate in predicates:
                 try:
                     predicate(*args, **kwargs)
-                except ConstraintError as exc:
+                except (ConstraintError, ConstraintsErrors) as exc:
                     if handler is not None:
                         return handler(exc)
                     raise
